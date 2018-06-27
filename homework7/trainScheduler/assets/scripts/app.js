abd4a67a399f9183e95e1e5a2d9ce8e27896c1ed
Jason Erdahl <jrerdahl@mmm.com>
2018-06-27 09:20:56 -0500

    updates

--- homework7/trainScheduler/assets/scripts/app.js
+++ homework7/trainScheduler/assets/scripts/app.js
@@ -1,14 +1,4 @@
-/* global firebase moment */
-// Steps to complete:
 
-// 1. Initialize Firebase
-// 2. Create button for adding new Trains - then update the html + update the database
-// 3. Create a way to retrieve Trains from the Train database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDKFfJF_faqFCviogn94go02fmSzJcSkTA",
