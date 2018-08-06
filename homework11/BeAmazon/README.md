3502c90c6b1b33fd9a76e04d43b81751d7c27cf0
Jason Erdahl <jasonerdahl@gmail.com>
2018-08-06 08:47:07 -0500

    update homework12

--- homework11/BeAmazon/README.md
+++ homework11/BeAmazon/README.md
@@ -1,5 +1,13 @@
 Node.js & MySQL
 
+https://drive.google.com/file/d/1kyxVRy3YNNPWUEqaGFXQBSeJSVnd4o7n/view
+
+https://jasonerdahl.github.io/portfolio.html
+
+https://github.com/JasonErdahl/JasonErdahl.github.io/tree/master/homework11
+
+https://github.com/JasonErdahl/JasonErdahl.github.io/blob/master/homework11/BeAmazon/README.md
+
 Overview
 
 In this activity, you'll be creating an Amazon-like storefront with the MySQL skills you learned this week. The app will take in orders from customers and deplete stock from the store's inventory. As a bonus task, you can program your app to track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
---
+++ homework12/test.js
@@ -0,0 +1,55 @@
+
+var friendsArray = [
+    {
+    "name":"Sophia",
+    "photo":"https://i.pinimg.com/564x/30/af/a5/30afa55b122bfbf97f2cc09dbdee48a6.jpg",
+    "scores":[
+        "3",
+        "5",
+        "4",
+        "2",
+        "5",
+        "2",
+        "4",
+        "3",
+        "2",
+        "5"
+        ]
+    },
+    {
+    "name": "Becky",
+    "photo": "https://i.pinimg.com/564x/51/95/d4/5195d43a3ffd45a5106d94dd8a32bbaf.jpg",
+    "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "5",
+        "1",
+        "4",
+        "3",
+        "1",
+        "4"
+    ]
+    },
+    {
+    "name": "Amy",
+    "photo": "https://i.pinimg.com/564x/d8/77/ff/d877ffaad803cced5dc61eb6d4b5f1e9.jpg",
+    "scores": [
+        "3",
+        "5",
+        "4",
+        "2",
+        "3",
+        "1",
+        "4",
+        "3",
+        "2",
+        "3"
+    ]
+    }
+];
+var scoresArray = [];
+console.log(friendsArray.length);
+
+//currentName: Amy currentPhoto: https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg
